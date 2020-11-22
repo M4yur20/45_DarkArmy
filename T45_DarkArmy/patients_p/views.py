eff83470385d820a8170cdea089ff943ce9449d2
Mayur Agarwal <51584569+M4yur20@users.noreply.github.com>
2020-11-22 10:00:18 +0530

    Update views.py

--- T45_DarkArmy/patients_p/views.py
+++ T45_DarkArmy/patients_p/views.py
@@ -7,6 +7,6 @@ from notifications.models import *
 # Create your views here.
 
 def patprof(request):
-    patient=Patient.objects.get(user=request.user)
+    patient = Patient.objects.get(user=request.user)
     notification = Notification.objects.filter(patient=patient)
     return render(request, 'patients_p/patientp.html', {'patient': patient, 'noti': notification})

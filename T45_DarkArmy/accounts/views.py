00dca5296d2c76c774855562274a4c16168e9248
JoshuaGodinho <69786791+JoshuaGodinho@users.noreply.github.com>
2020-11-22 07:33:53 +0530

    commit

--- T45_DarkArmy/accounts/views.py
+++ T45_DarkArmy/accounts/views.py
@@ -12,6 +12,7 @@ from geopy.distance import geodesic
 from .utils import get_geo, get_center_coordinates, get_zoom
 from django.contrib.auth.decorators import user_passes_test, login_required
 from notifications.models import Notification
+'''
 import cv2
 
 import numpy as np
@@ -20,7 +21,7 @@ from keras.losses import categorical_crossentropy
 from keras.optimizers import Adam
 
 labels = ['Bite', 'Burns', 'Cuts', 'Fractures']
-
+'''
 
 def nearesthosps(request):
     geolocator = Nominatim(user_agent='accounts')
